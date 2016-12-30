13d777aee53520acddefbac05334de1741a6a71b
Alexandre Andrade <kaniabi@gmail.com>
2016-12-30 08:21:56 -0200

    Printing JNLP/SWARM version.

--- swarm-client.sh
+++ swarm-client.sh
@@ -5,5 +5,6 @@ function _run-it {
     exec /sbin/setuser jenkins-slave $1
 }
 
+echo "INFO: SWARM version: $JENKINS_SWARM_VERSION"
 JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
 _run-it "/usr/bin/java $JAVA_OPTS -jar $JAR -fsroot $JENKINS_SLAVE_HOME $JENKINS_SLAVE_PARAMS"
